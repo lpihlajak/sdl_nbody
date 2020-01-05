a7fb8ba314808317494313988d4324c51d71c1c2
lauri <lauri.pihlajakangas@gmail.com>
2020-01-06 00:51:27 +0200

    release version

--- main.c
+++ main.c
@@ -8,15 +8,15 @@
 
 #define PIXELSIZE 4
 
-#define TOTAL_FRAMES 2400
+#define TOTAL_FRAMES 6000
 
 #define COLOR_INTENSITY 2000
 
-#define GRAV_LIMIT 111110.5
+#define GRAV_LIMIT 10.0
 
 float metaCalc(float x, float y)
 {
-    return ((float)1.0 / (x * x + y * y));
+    return 1.0/(x * x + y * y);
 }
 
 struct Vec2 {
@@ -28,6 +28,14 @@ typedef struct Meta {
     struct Vec2 pos;
     struct Vec2 v;  // Velocity
 } Meta;
+
+uint8_t leik(float x)
+{
+    if (x > 255.0)
+        return 255;
+    else
+        return (uint8_t)x;
+}
 
 void updateForces(struct Meta meta[3])
 {
@@ -56,7 +64,7 @@ void updateForces(struct Meta meta[3])
 
             float gravity;
             
-            if((fabsf(xDelta) > 10) && (fabsf(yDelta) > 10))
+            if((fabsf(xDelta) > 15) && (fabsf(yDelta) > 15))
             {               
                 gravity = 1.0 / ( sqrt((xDelta * xDelta + yDelta * yDelta))); // distance sqrt( (x2-x1)^2 + (y2-y1)^2 ) 
             }
@@ -125,17 +133,17 @@ int main()
     struct Meta meta[3];
 
     meta[0].pos.x = WIDTH/2;
-    meta[0].pos.y = HEIGHT/2-100;
+    meta[0].pos.y = HEIGHT/2-50;
     meta[0].v.x = 0.0;
     meta[0].v.y = 0.0;
 
-    meta[1].pos.x = WIDTH/2+60;
-    meta[1].pos.y = HEIGHT/2;
+    meta[1].pos.x = WIDTH/2+100;
+    meta[1].pos.y = HEIGHT/2+100;
     meta[1].v.x = 0.0;
     meta[1].v.y = 0;
 
-    meta[2].pos.x = WIDTH/2;
-    meta[2].pos.y = HEIGHT/2+80;
+    meta[2].pos.x = WIDTH/2-100;
+    meta[2].pos.y = HEIGHT/2+100;
     meta[2].v.x = 0.0;
     meta[2].v.y = 0.0;
 
@@ -145,11 +153,27 @@ int main()
 
     //printf("hello");
 
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer); //SDL_WINDOW_FULLSCREEN
+    SDL_AudioSpec wavSpec;
+    Uint32 wavLength;
+    Uint8* wavBuffer;
+
+    SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO);
+    SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, SDL_WINDOW_FULLSCREEN, &window, &renderer); //
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    SDL_LoadWAV("tune.wav", &wavSpec, &wavBuffer, &wavLength);
+
+    SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
+
+    int success = SDL_QueueAudio(deviceId, wavBuffer, wavLength);
+    SDL_PauseAudioDevice(deviceId, 0);
+
+    //SDL_Delay(3000);
+
+ 
+    //return 0;
 
     //clear(buffer);
 
@@ -171,21 +195,21 @@ int main()
                 amplitude2 = metaCalc(x - meta[1].pos.x, y - meta[1].pos.y);
                 amplitude3 = metaCalc(x - meta[2].pos.x, y - meta[2].pos.y);
 
+                //float color = ((float)0xFF * amplitudeSum * 20000);
                 amplitudeSum = amplitude1 + amplitude2 + amplitude3;
-                float color = ((float)0xFF * amplitudeSum * 20000);
-               
-                if(amplitudeSum > 0.0005)
+                if(amplitudeSum > 0.00025)
                 {
-                    SDL_SetRenderDrawColor(renderer, (uint8_t)(20000.0 * amplitude1 ),
-                        (uint8_t)(20000.0 * amplitude2),
-                        (uint8_t)(20000.0 * amplitude3),
-                        255);
-                    
                     //SDL_SetRenderDrawColor(renderer, 
-                    //    0xFF,
-                    //    0xFF,
-                    //   0xFF,
+                    //    (uint8_t)(255.0 * (amplitude1-(int)amplitude1)),
+                    //    (uint8_t)(255.0 * (amplitude2-(int)amplitude2)),
+                    //    (uint8_t)(255.0 * (amplitude3-(int)amplitude3)),
                     //    255);
+                    
+                    SDL_SetRenderDrawColor(renderer, 
+                        leik(amplitude1*400000.0) ,
+                        leik(amplitude2*400000.0) ,
+                        leik(amplitude3*400000.0) ,
+                        255);
                         
                    // SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
                     SDL_RenderDrawPoint(renderer, x, y);
@@ -198,9 +222,13 @@ int main()
 
         updateForces(meta);
 
-        if (SDL_PollEvent(&event) && ((event.type == SDL_QUIT) || (event.key.keysym.sym == SDLK_ESCAPE)) )
+        if ((SDL_PollEvent(&event) && ((event.type == SDL_QUIT) || (event.key.keysym.sym == SDLK_ESCAPE)) ) || (SDL_GetTicks() > 60000) )
             break;
     }
+
+    SDL_CloseAudioDevice(deviceId);
+    SDL_FreeWAV(wavBuffer);
+
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
